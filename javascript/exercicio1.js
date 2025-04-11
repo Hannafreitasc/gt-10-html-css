9631ef368de54ddfaea4d3aabadea37a7ec17489
hannafreitasc <hannafreitascc@gmail.com>
2025-04-11 11:55:33 -0300

    javascript-aula-1

---
+++ javascript/exercicio1.js
@@ -0,0 +1,18 @@
+const nome = "Hanninha" //texto - String 
+const idade = 24 //número - number
+const altura = 1.40  //número -number
+const maiorDeIdade = true //booleano - booelan
+
+console.log ("Olá seu nome é " + nome) //concatenação
+
+console.log ("Olá seu nome é " + nome + " você tem " + idade + " sua altura é " + altura )
+
+console.log ("Olá você é maior de idade? "  + maiorDeIdade)
+
+
+//Template string ou string dinamica
+const frase = `Olá seu nome é ${nome}`
+
+console.log(frase)
+
+console.log(10+11)
---
+++ javascript/exercicio5.js
@@ -0,0 +1,14 @@
+//funcao receberá dias alugados
+//o valor por dia é 250
+
+function calcularAluguel(dias){
+    const valorPordia = 250 
+
+    const calculo = dias * valorPordia
+    console.log(calculo)
+}
+
+    calcularAluguel(20)
+
+    
+
